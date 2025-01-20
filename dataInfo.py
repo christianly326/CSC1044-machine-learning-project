884407a70b86a31256883b6dbdee69f4534a10f2
christianly326 <christianyacapin@gmail.com>
2025-01-20 12:43:42 +0000

    Data manipulation file added

--- dataInfo.py
+++ dataInfo.py
@@ -4,9 +4,14 @@ import pandas as pd
 df = pd.read_csv('dataset/Training_set.csv')
 
 dict = {}
-for label in df['label']:
+
+dict_names = {}
+for idx, label in enumerate(df['label']):
     dict[label] = dict.get(label, 0) + 1
-    
+    if label not in dict_names:
+        dict_names[label] = []
+    else:
+        dict_names[label].append(df['filename'][idx])
 lowest = max(dict.values())
 highest = min(dict.values())
 lowest_key = ""
@@ -18,8 +23,13 @@ for k in dict.keys():
     if highest < dict[k]:
         highest = dict[k]
         highest_key = k
-    print(k, dict[k])
+# print(k, dict[k])
 
-print("Lowest:\n", lowest_key, lowest)
-print("Highest:\n", highest_key, highest)
-print("Average:\n", sum(dict.values())/len(dict))
+# print("Lowest:\n", lowest_key, lowest)
+# print("Highest:\n", highest_key, highest)
+# print("Average:\n", sum(dict.values())/len(dict))
+
+# print("Amount of classes\n", len(dict))
+
+for k, v in dict_names.items():
+    print(k, v[:10], "\n")
